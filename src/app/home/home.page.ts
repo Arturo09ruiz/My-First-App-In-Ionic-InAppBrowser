1fc54db2a7d2136e8c7af5f507cdcf726e499f1a
Gregory <gregory09ruiz@gmail.com>
2020-05-31 23:01:25 -0400

    Version 1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 
 
 @Component({
@@ -11,8 +11,49 @@ export class HomePage {
 
   constructor(private browser:InAppBrowser) {
 
-    this.browser.create('http://localhost/proyectos-con-henry/DFM/', '_self');
-
   }
+
+
+ moi(){
+
+  let options: InAppBrowserOptions = {
+    fullscreen: 'yes' ,
+    hidden: 'yes',
+    hidenavigationbuttons: 'yes',
+    hidespinner: 'yes' , 
+    hideurlbar: 'yes' , 
+    toolbar: 'no',
+  } 
+
+
+  this.browser.create('https://touch.facebook.com/moises.serrano.3956', '_self', options);
+
+ }
+ 
+ hen(){
+
+  let options: InAppBrowserOptions = {
+    fullscreen: 'yes' ,
+    hidden: 'yes',
+    hidenavigationbuttons: 'yes',
+    hidespinner: 'yes' , 
+    hideurlbar: 'yes' , 
+    toolbar: 'no',
+  } 
+
+
+  this.browser.create('https://touch.facebook.com/hnery', '_self', options);
+
+ }
+
+
+
+
+
+
+
+
+
+
 
 }
